5f47e66ffe6aac9a40e183422f62be54fdec1fee
Philip Cesar B. Garay <philipgaray2@gmail.com>
2015-09-11 13:25:03 +0800

    Initial Commit

---
+++ iOSTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSTest
+//
+//  Created by Philip Cesar Garay on 9/11/15.
+//  Copyright (c) 2015 Philip Cesar Garay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSTest
+//
+//  Created by Philip Cesar Garay on 9/11/15.
+//  Copyright (c) 2015 Philip Cesar Garay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
